3434bc11a7047496c95126d07a7509f599a46e79
Your Name <you@example.com>
2024-08-20 10:39:54 +0200

    init

---
+++ src/testpoc.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace pearpearphpnet\netsocket2;
+
+class testpoc
+{
+    /**
+     * Example method that returns a greeting message.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function greet(string $name): string
+    {
+        return "Hello, " . $name . "! Welcome to mypackage.";
+    }
+}
